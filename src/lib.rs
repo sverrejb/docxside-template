1071f4960aa435d727538c8542dc103e1eccd57a
Sverre Johann Bjørke <sverrejb@gmail.com>
2024-07-04 22:24:29 +0200

    type fields work, kinda

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,21 @@
+use docx_rs::read_docx;
+use docx_rs::Docx;
+use file_format::FileFormat;
+use heck::AsPascalCase;
+use heck::AsSnakeCase;
 use std::{
     env,
     ffi::OsString,
-    fs::{self, OpenOptions},
-    io::Write,
+    fs::{self, File, OpenOptions},
+    io::{Read, Write},
     path::Path,
 };
 
-use file_format::FileFormat;
-use heck::AsPascalCase;
-
 const TYPE_TEMPLATE: &str = "
 pub struct {name} {
-    v1: String,
-    v2: String,
-    v3: String,
+    [props]
 }
 ";
-
-//TODO: Function to include types in consumer.
 
 #[macro_export]
 macro_rules! include_templates {
@@ -36,10 +34,27 @@ fn remove_extension(filename: &str) -> String {
 fn generate_type_name(filename: OsString) -> Result<String, String> {
     if let Ok(file_name_string) = filename.into_string() {
         let mut type_name = remove_extension(file_name_string.as_str());
+        type_name = type_name
+            .trim_start_matches(|c: char| c.is_numeric())
+            .to_string();
         type_name = format!("{}", AsPascalCase(type_name));
         return Ok(type_name);
     }
     Err("Could not convert filename to string".to_owned())
+}
+
+fn get_props(doc: &Docx) -> String {
+    let props = &doc.doc_props.custom.properties;
+    let mut fields_str = String::new();
+    for (key, _) in props {
+        let rust_type = "String";
+        //TODO: handle all illegal characters
+        let mut field_name = key.replace(" ", "_");
+        field_name = field_name.replace(":", "_");
+        field_name = format!("{}", AsSnakeCase(field_name));
+        fields_str.push_str(&format!("{}: {},\n", field_name, rust_type));
+    }
+    fields_str
 }
 
 pub fn generate_types(template_path: &str) {
@@ -57,21 +72,37 @@ pub fn generate_types(template_path: &str) {
         .unwrap();
 
     if let Ok(files) = fs::read_dir(template_dir) {
-        for file in files {
-            let f = file.as_ref().unwrap();
-            let fmt = FileFormat::from_file(f.path()).unwrap();
+        for dir_entry in files {
+            let file_path = dir_entry.as_ref().unwrap().path();
+            let fmt = FileFormat::from_file(&file_path).unwrap();
             // ignore non-docx files
             if fmt.extension() != "docx" {
                 continue;
             }
 
-            //TOOD: handle errors
-            if let Ok(type_name) = generate_type_name(file.unwrap().file_name()) {
-                let formatted_string = TYPE_TEMPLATE.replace("{name}", type_name.as_str());
-                generated_types_file
-                    .write_all(formatted_string.as_bytes())
-                    .unwrap();
+            match File::open(file_path) {
+                Ok(mut file) => {
+                    let mut buf = vec![];
+                    let _ = file.read_to_end(&mut buf);
+                    let doc = read_docx(&buf).unwrap();
+
+                    if let Ok(type_name) = generate_type_name(dir_entry.unwrap().file_name()) {
+                        let mut formatted_string =
+                            TYPE_TEMPLATE.replace("{name}", type_name.as_str());
+                        formatted_string =
+                            formatted_string.replace("[props]", get_props(&doc).as_str());
+                        generated_types_file
+                            .write_all(formatted_string.as_bytes())
+                            .unwrap();
+                    }
+                }
+
+                Err(e) => {
+                    println!("Error opening file: {}", e);
+                }
             }
+
+            //TOOD: handle errors
         }
     }
 }
