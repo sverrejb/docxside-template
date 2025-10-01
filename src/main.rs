f7f0a9f91761ad31e61a7e632533b64468eb036c
Sverre Johann Bjørke <sverrejb@gmail.com>
2025-10-01 11:23:17 +0200

    exiting work mac

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use docxside_templates::generate_templates;
 generate_templates!("test");
 
 fn main() {
+
     let instance = HelloWorld {
         name: "Sverre".into(),
         test: "hehe".into(),
@@ -14,9 +15,8 @@ fn main() {
     println!("{:?}", instance);
     println!("{:?}", instance.get_file_path());
 
-    match instance.save("./foo/testfest.docx") {
+    match instance.save("./foo/skuta_meg.docx") {
         Ok(_) => println!("SAVED"),
         Err(err) => println!("FAILED: {}", err),
     }
-
 }
